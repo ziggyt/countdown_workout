304b5b26aea3410d32253f63c6e884489b66fb51
ziggyt <frmax@student.chalmers.se>
2020-10-14 16:36:50 +0200

    Add AppBar to main

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,30 @@ import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
   runApp(NeumorphicApp(
-    home: Home(),
-
-  ));
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFFFFFFF),
+        lightSource: LightSource.topLeft,
+        depth: 10,
+      ),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        depth: 6,
+      )));
 }
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-    );
+    return Scaffold(
+        appBar: NeumorphicAppBar(
+          title: Text('Workout Countdown'),
+          centerTitle: true,
+        ),
+        backgroundColor: NeumorphicTheme.baseColor(context));
   }
 }
-
-
